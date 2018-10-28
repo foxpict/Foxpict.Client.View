fd30ac679abe86d9f5cad6f179f4f7db33f29a8e
atachimiko <atachi.miko@gmail.com>
2018-10-28 23:34:49 +0900

    Toggle未定義エラーが出る問題を修正

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
 
     NgbModule.forRoot(),
